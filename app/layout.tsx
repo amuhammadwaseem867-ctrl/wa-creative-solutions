a859c95ff30b6b618a35254fbe51ceac4d669eee
Muhammad Waseem Akram <amuhammadwaseem867@gmail.com>
2026-08-16 10:39:01 -0700

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import AIChat from "@/components/AIChat";
 
@@ -31,6 +32,8 @@ export default function RootLayout({
         {children}
 
         <AIChat />
+
+        <SpeedInsights />
       </body>
     </html>
   );
